1797c2172823304735b5987dcff26c42df6bd84a
nicomarmo <marmonicolas10@gmail.com>
2022-07-28 19:02:27 -0300

    props bien2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
-const props =
-  {
-    greeting: 'Hola!'
-  }
-
 
 function App() {
 const styles = {marginLeft: "200px", color: "red"};
